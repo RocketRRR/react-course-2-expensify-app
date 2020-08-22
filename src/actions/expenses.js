f02232a7601ecf4696c1c3d051d649498fb15487
Steve <animationsforsteve@gmail.com>
2020-08-22 17:46:38 -0400

    Add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -47,13 +47,20 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+      dispatch(editExpense(id, updates));
+    });
+  };
+};
+
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
   expenses
 });
 
-// export const startSetExpenses;
 export const startSetExpenses = () => {
   return (dispatch) => {
     return database.ref('expenses').once('value').then((snapshot) => {
